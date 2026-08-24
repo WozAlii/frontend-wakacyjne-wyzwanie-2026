81a275ed704ddeb3ae80d75572daca424f2be693
Alicja <alicja.woznica26@gmail.com>
2026-08-24 22:52:47 +0200

    feat:add Next.js components to TeamMemberCard

--- rozdzial-3-komponenty-nextowe/src/components/TeamMemberCard.tsx
+++ rozdzial-3-komponenty-nextowe/src/components/TeamMemberCard.tsx
@@ -1,3 +1,12 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Link from "next/link";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
 interface TeamMemberCardProps {
   name: string;
   role: string;
@@ -14,21 +23,18 @@ export default function TeamMemberCard({
   return (
     <div className="flex w-full max-w-md flex-col overflow-hidden rounded-2xl bg-white shadow-lg sm:max-w-xl sm:flex-row">
       <div className="flex h-44 items-center justify-center bg-red-400 sm:h-auto sm:w-48">
-        <div className="flex h-24 w-24 m-5 items-center justify-center rounded-full bg-black/20 text-3xl font-bold text-white">
-          {
-            name
-              .split(" ")
-              .map((w) => w[0])
-              .join("")
-              .slice(0, 2)
-              .toUpperCase()
-          }
-        </div>
+        <Image
+          loading="eager"
+          src="/profile-default.svg"
+          width={500}
+          height={500}
+          alt="Default profile picture of the author"
+        />
       </div>
       <div className="flex flex-col gap-4 p-6">
         <div>
           <h2 className="text-xl font-bold text-gray-900">{name}</h2>
-          <p className="mt-1 text-sm font-medium text-red-700 font-mono">{role}</p>
+          <p className={inter.className}>{role}</p>
         </div>
         <p className="text-sm leading-6 text-gray-600">{bio}</p>
         <div>
@@ -46,6 +52,15 @@ export default function TeamMemberCard({
             ))}
           </ul>
         </div>
+
+        <Link
+          href="https://solvro.pwr.edu.pl/pl/"
+          target="_blank"
+          rel="noreferrer"
+          className="self-start rounded-full bg-blue-200 px-3 py-1 text-xs font-medium text-gray-700 shadow-md transition hover:bg-blue-400 hover:shadow-lg"
+        >
+          Dowiedz się więcej
+        </Link>
       </div>
     </div>
   );
